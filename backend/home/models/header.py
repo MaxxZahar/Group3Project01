983a16572741fc526ec345a10da67098f911c126
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-06-14 15:43:00 +0300

    blank and null

--- backend/home/models/header.py
+++ backend/home/models/header.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class HeaderModel(models.Model):
-    phone = models.CharField(max_length=30, verbose_name='Телефон')
-    phone_url = models.CharField(max_length=30, verbose_name='Телефон ссылка')
+    phone = models.CharField(blank=True, null=True, max_length=30, verbose_name='Телефон')
+    phone_url = models.CharField(blank=True, null=True, max_length=30, verbose_name='Телефон ссылка')
     logo = models.FileField(verbose_name='Логотип')
     logo_alt = models.CharField(max_length=255, verbose_name='Описание логотипа')
 
--- backend/home/models/top_block.py
+++ backend/home/models/top_block.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class TopBlockModel(models.Model):
     title = models.TextField(verbose_name='Заголовок')
-    top_right_text = models.CharField(max_length=255, verbose_name='Текст в верхнем правом углу')
-    text = models.TextField(verbose_name='Текст')
-    img = models.FileField(verbose_name='Главное изображение')
-    img_alt = models.CharField(max_length=255, verbose_name='Описание изображения')
-    logo = models.FileField(verbose_name='Логотип')
-    logo_alt = models.CharField(max_length=255, verbose_name='Описание логотипа')
+    top_right_text = models.CharField(blank=True, null=True, max_length=255, verbose_name='Текст в верхнем правом углу')
+    text = models.TextField(blank=True, null=True, verbose_name='Текст')
+    img = models.FileField(blank=True, null=True, verbose_name='Главное изображение')
+    img_alt = models.CharField(blank=True, null=True, max_length=255, verbose_name='Описание изображения')
+    logo = models.FileField(blank=True, null=True, verbose_name='Логотип')
+    logo_alt = models.CharField(blank=True, null=True, max_length=255, verbose_name='Описание логотипа')
 
     def __str__(self):
         return self.title
