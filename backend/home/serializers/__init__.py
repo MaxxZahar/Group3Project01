bf47cf0f6ef09ba35345ed17fc3c7b2e2f48aabd
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-06-18 12:49:14 +0300

    implementation serializers

--- backend/home/serializers/__init__.py
+++ backend/home/serializers/__init__.py
@@ -1,3 +1,5 @@
 from .top_block import TopBlockSerializer
 from .header import HeaderSerializer
 from .description_block import DescriptionBlockSerializer, ProblemSerializer, ProblemPartSerializer
+from .implementation_block import ImplementationBlockSerializer, FunctionalityColumnSerializer,\
+    FunctionalitySerializer, BottomSlideSerializer
