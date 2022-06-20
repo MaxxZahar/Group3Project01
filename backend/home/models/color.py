f5bab2a0248fdb676d200a063358c07c105e7b4e
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-06-21 02:22:57 +0300

    color model update

--- backend/home/models/color.py
+++ backend/home/models/color.py
@@ -4,7 +4,8 @@ from app.fields import ColorField
 
 class ColorModel(models.Model):
     default = models.BooleanField(default=True, verbose_name='Оставить настройки по умолчанию')
-    color = ColorField(default='#ffffff', verbose_name='Выбрать цвет')
+    color = ColorField(default='#4036ba', verbose_name='Выбрать цвет')
+    description_font_color = ColorField(default='#4036ba', verbose_name='Выбрать цвет текста в блоке описания')
 
     def __str__(self):
         return self.color
