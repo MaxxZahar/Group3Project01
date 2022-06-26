a56a4ded5bb1cd1186b3459ee322b4fea09809ca
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-06-27 02:51:53 +0300

    color model upgrade

--- backend/home/models/color.py
+++ backend/home/models/color.py
@@ -6,6 +6,7 @@ class ColorModel(models.Model):
     default = models.BooleanField(default=True, verbose_name='Оставить настройки по умолчанию')
     color = ColorField(default='#4036ba', verbose_name='Выбрать цвет')
     description_font_color = ColorField(default='#4036ba', verbose_name='Выбрать цвет текста в блоке описания')
+    secondary_color = ColorField(default='#24d7ff', verbose_name='Выбрать второй цвет')
 
     def __str__(self):
         return self.color
