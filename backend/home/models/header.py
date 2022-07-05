9a27ca59185249a03d85b018b89c07e382a5bf5e
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-07-05 12:59:38 +0300

    extended logo add

--- backend/home/models/header.py
+++ backend/home/models/header.py
@@ -6,6 +6,9 @@ class HeaderModel(models.Model):
     phone_url = models.CharField(blank=True, null=True, max_length=30, verbose_name='Телефон ссылка')
     logo = models.FileField(verbose_name='Логотип')
     logo_alt = models.CharField(max_length=255, verbose_name='Описание логотипа')
+    extended_logo = models.FileField(blank=True, null=True, verbose_name='Логотип для развёрнутого меню')
+    extended_logo_alt = models.CharField(blank=True, null=True, max_length=255,
+                                         verbose_name='Описание логотипа для развёрнутого меню')
 
     def __str__(self):
         return 'Телефон и логотип для верхушки сайта'
--- backend/home/serializers/header.py
+++ backend/home/serializers/header.py
@@ -6,6 +6,7 @@ from django.core.validators import FileExtensionValidator
 
 class HeaderSerializer(serializers.ModelSerializer):
     logo = serializers.FileField(validators=[FileExtensionValidator(ALLOWED_IMAGE_EXTENSIONS)])
+    extended_logo = serializers.FileField(validators=[FileExtensionValidator(ALLOWED_IMAGE_EXTENSIONS)])
 
     class Meta:
         model = HeaderModel
