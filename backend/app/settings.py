32fad78c271aa43d73c1c1fb1ba813bf26fcb6d4
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-07-05 12:43:02 +0300

    extended menu

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -7,10 +7,18 @@ INSTALLED_APPS += [ # noqa
 ]
 
 MENU_TYPE_HEADER_MENU = 'header_menu'
+MENU_TYPE_EXTENDED_MENU = 'extended_menu'
+MENU_TYPE_EXTENDED_SUBMENU = 'extended_submenu'
 
 MENU_TYPES = {
     MENU_TYPE_HEADER_MENU: {
         'title': 'Верхнее меню',
+    },
+    MENU_TYPE_EXTENDED_MENU: {
+        'title': 'Развёрнутое меню',
+    },
+    MENU_TYPE_EXTENDED_SUBMENU: {
+        'title': 'Развёрнутое подменю',
     },
 }
 
